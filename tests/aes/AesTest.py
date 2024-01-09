94117551bcec6d6b56bf9950aacc795785c85664
GodsApostles838 <blake.digi123@gmail.com>
2024-01-10 03:43:18 +1100

    Deleted comments

--- tests/aes/AesTest.py
+++ tests/aes/AesTest.py
@@ -1,12 +1,9 @@
 from AES_encryption import AES_EASYcrypt
 
-# Replace 'your_secret_key_here' with your actual key
 key = b'Donuts'
 
-# Create an instance of AES_EASYcrypt
 easy_crypt = AES_EASYcrypt(key)
 
-# Example: Encrypt a message
 message_to_encrypt = "Hello, this is a secret message!"
 encrypted_message = easy_crypt.encrypt_m(message_to_encrypt)
 
